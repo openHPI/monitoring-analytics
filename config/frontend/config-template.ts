d5fc62c8c63babab5e2fb0b6035245cf11058fd2
Hendrik Folkerts <hendrik.folkerts@student.hpi.uni-potsdam.de>
2019-03-14 13:03:00 +0100

    Add kapacitor URL to config template

--- config/frontend/config-template.ts
+++ config/frontend/config-template.ts
@@ -1,5 +1,6 @@
 export default {
     baseURL: 'https://www.example.com',
     kapacitorProxyURL: 'https://www.example.com:8082',
+    kapacitorURL: 'https://www.example.com:9092',
     platforms: ['platform-a', 'platform-b'],
 };
